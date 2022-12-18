fc5c8597d44500db6e5e0a20c5d92558d5e0f715
baranzeyn <baranzeyn676@gmail.com>
2022-12-18 13:26:20 +0300

    fibonacci odevi eklendi

---
+++ fibo/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+	
+		static int fibo(int a) {
+	if(a==1 || a==2) {
+		return 1;
+		
+	}
+	return fibo(a-1) + fibo(a-2);
+			
+		}
+	public static void main(String[] args) {
+		System.out.println(fibo(5));
+
+	}
+
+}
